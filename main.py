1821fb802175b099b2a52a732eeec38c2657d201
mafefreeitas <128387890+mafefreeitas@users.noreply.github.com>
2023-05-07 21:34:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
         if n < 0:
             raise ValueError("não existe fibonacci com n negativo!")
         if type(n) != int:
-            raise ValueError("só serve com números inteiros")
+            raise ValueError("só serve com numeros inteiros")
         fibonacci =[0,1]
         if n ==0:
             return fibonacci[0]
